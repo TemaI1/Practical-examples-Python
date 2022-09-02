70b34d607329b56c68b3fb34a0889b5ec0116600
Artem <xTema05x@mail.ru>
2022-09-02 15:18:54 +0300

    принимает 5 чисел и находит максимальное из них

---
+++ Practical002.py
@@ -0,0 +1,51 @@
+# Напишите программу, которая на вход принимает 5 чисел
+# и находит максимальное из них
+# Примеры:
+# - 1, 4, 8, 7, 5 -> 8
+# - 78, 55, 36, 90, 2 -> 90
+
+
+
+
+
+# num_1 = int(input('Введите число 1: '))
+# num_2 = int(input('Введите число 2: '))
+# num_3 = int(input('Введите число 3: '))
+# num_4 = int(input('Введите число 4: '))
+# num_5 = int(input('Введите число 5: '))
+
+# max = num_1
+# if num_2 > max: 
+#     max = num_2
+# if num_3 > max: 
+#     max = num_3
+# if num_4 > max: 
+#     max = num_4
+# if num_5 > max: 
+#     max = num_5
+
+# print(max)
+
+
+
+
+
+# numbers = [1, 4, 8, 7, 5]
+
+# max_num = numbers[0] 
+# for i in numbers[1:]:
+#     if i > max_num:
+#         max_num = i
+# print(max_num)
+
+
+
+
+
+# numbers = [3, 98, 7, 10, 45]
+# print(max(numbers))
+
+
+
+
+
