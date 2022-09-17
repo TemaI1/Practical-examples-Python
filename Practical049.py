42dec12a215970287567a9ed82917626b60894e5
Artem <xTema05x@mail.ru>
2022-09-17 20:20:39 +0300

    Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.

---
+++ Practical049.py
@@ -0,0 +1,20 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+code = input('Введите символы: ')
+result = str()
+
+i = 0
+while i < len(code):
+    count = 1
+    while i + 1 < len(code) and code[i] == code[i + 1]:
+        count = count + 1
+        i += 1
+    result += str(count) + code[i]
+    i += 1
+
+with open("input_049.txt", "w") as input_049:
+    print(f'input data: {code}', file=input_049)
+
+with open("output_049.txt", "w") as output_049:
+    print(f'output data: {result}', file=output_049)
