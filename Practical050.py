9eff3e916d0e1cb9b34ea45e79ad380aa68edcc3
Artem <xTema05x@mail.ru>
2022-09-22 18:46:11 +0300

    вычисления арифметического выражения заданного строкой

---
+++ Practical050.py
@@ -0,0 +1,50 @@
+# Напишите программу вычисления арифметического выражения заданного строкой.
+# Используйте операции +,-,/,*. приоритет операций стандартный. 
+# *Пример:* 
+# 2+2 => 4;
+# 1+2*3 => 7;
+# 1-2*3 => -5;
+# Добавьте возможность использования скобок, меняющих приоритет операций.
+# *Пример:* 
+# 1+2*3 => 7; 
+# (1+2)*3 => 9;
+
+
+# num = str((1+2)*3)
+# result = int(num)
+# print(f'{result}')
+
+# a = ['412', '*', '123', '+', '412']
+# a = list(map(lambda x: int(x) if x.isdigit() else x, a))
+# print(a)
+
+string = '1 + 2 * 3 + 8 / 2'.split()
+for i in range(len(string)):
+    if string[i].isdigit():
+        string[i] = int(string[i])
+op_1 = {'+': lambda x, y: x + y,
+        '-': lambda x, y: x - y}
+op_2 = {'*': lambda x, y: x * y,
+        '/': lambda x, y: x / y}
+
+index = 0
+while ('*' in string) or ('/' in string):
+    if string[index] in op_2:
+        temp = op_2[string[index]](string[index - 1], string[index + 1])
+        del string[index - 1:index + 2]
+        string.insert(index - 1, temp)
+        index = 0
+    else:
+        index += 1
+
+index = 0
+while'+' in string or '-' in string:
+    if string[index] in op_1:
+        temp = op_1[string[index]](string[index - 1], string[index + 1])
+        del string[index - 1:index + 2]
+        string.insert(index - 1, temp)
+        index = 0
+    else:
+        index += 1
+
+print(string)
