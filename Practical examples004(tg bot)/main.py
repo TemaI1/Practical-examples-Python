7d33859c1706531ecf85636563393d8601aada6a
Artem <xTema05x@mail.ru>
2022-10-03 15:29:34 +0300

    tg bot

--- Practical examples004(tg bot)/main.py
+++ Practical examples004(tg bot)/main.py
@@ -1,13 +1,12 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
+from bot_commands import *
 
 app = ApplicationBuilder().token("5725809687:AAHJbwvQ5P8CbPLt7HHGbgMBONhNh96yQJ0").build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("hello", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
 
 app.run_polling()
